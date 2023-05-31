21090438f0757a7ad760bdcf76de38449059028f
kyle1james <kyle1james@gmail.com>
2023-05-30 22:13:41 -0400

    meoow recycle

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 from crystal_ball import CrystalBall
 
 app = Flask(__name__, static_folder='static')
+
+app.config['SECRET_KEY'] = 'your-secret-key'  # For session
+
+crystal_ball = None
 
 @app.route('/')
 def home():
@@ -9,17 +13,21 @@ def home():
 
 @app.route('/ask', methods=['GET', 'POST'])
 def ask():
-    if request.method == 'POST':
-        crystal_ball = CrystalBall()
+    global crystal_ball
 
+    if request.method == 'POST':
         document = request.form['document']
         question = request.form['question']
         doc_type = request.form.get('doc_type', 'txt')  # Default doc_type to 'txt' if not provided
 
-        if doc_type.lower() == 'md':
-            document = crystal_ball.remove_markdown_syntax(document)
+        # Check if the document has changed or crystal_ball is None
+        if crystal_ball is None or document != session.get('document'):
+            crystal_ball = CrystalBall()
+            if doc_type.lower() == 'md':
+                document = crystal_ball.remove_markdown_syntax(document)
+            crystal_ball.document = document
+            session['document'] = document  # Store the document in session
 
-        crystal_ball.document = document
         answers = crystal_ball.answer_question(question)
 
         return render_template('show_answers.html', answers=answers)
@@ -32,4 +40,3 @@ def ask_again():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
